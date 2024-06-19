421e4d65a4e75909f36e0f3ed60fc11bab9819e5
dymanoid <9433345+dymanoid@users.noreply.github.com>
2024-06-19 12:26:11 +0200

    fix: use Unicode-aware SQLite filtering

--- packages/loot-core/src/server/aql/compiler.ts
+++ packages/loot-core/src/server/aql/compiler.ts
@@ -720,11 +720,11 @@ const compileOp = saveStack('op', (state, fieldRef, opData) => {
     }
     case '$like': {
       const [left, right] = valArray(state, [lhs, rhs], ['string', 'string']);
-      return `${left} LIKE ${right}`;
+      return `UNICODE_LIKE(${right}, ${left})`;
     }
     case '$notlike': {
       const [left, right] = valArray(state, [lhs, rhs], ['string', 'string']);
-      return `(${left} NOT LIKE ${right}\n OR ${left} IS NULL)`;
+      return `(NOT UNICODE_LIKE(${right}, ${left})\n OR ${left} IS NULL)`;
     }
     default:
       throw new CompileError(`Unknown operator: ${op}`);
